bb466e13a589b5a3b09e3485cd96614d266f1101
John Pearson <john.pearson@pacecademy.org>
2017-10-05 12:40:51 -0400

    Added in-file testing.

--- evenOdd.js
+++ evenOdd.js
@@ -10,3 +10,44 @@ function evenOdd(n) {
         console.log();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------------------------------------------------
+// Testing - DON'T CHANGE ANYTHING BELOW
+const printme = console.log;
+let out = [];
+console.log = function log(d) {
+    out.push(d);
+};
+evenOdd(2);
+printme('Testing...');
+printme(`\t did you properly return the user input?\t ${out[0] == 'You entered 2.'} `);
+const a = [3, 4];
+for (let i = 0; i < a.length; i += 1) {
+    out = [];
+    evenOdd(a[i]);
+    const t = a[i] % 2 == 0 ? 'even' : 'odd';
+    printme(`\t did you properly return a statement for ${t} values?\t ${out[1] == 'Your number is ' + t + '!'}`);
+}
